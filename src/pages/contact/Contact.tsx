6e18f3cc8f609c9cc287073e680aab51a4060671
dilshod <dilshodx516@gmail.com>
2025-02-26 11:31:39 +0500

    food contact update

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -38,6 +38,40 @@ const Contact = () => {
           </div>
         </div>
       </div>
+      <div className='flex flex-col items-center gap-20 mt-32 container mx-auto'>
+        <h2 className='text-center text-7xl text-black font-bold max-md:text-4xl'>
+          Write to us
+        </h2>
+        <form className='flex flex-col gap-5 items-end w-[800px] max-lg:w-full'>
+          <input
+            className='w-full h-14 bg-transparent border-2 rounded-xl text-black text-xl px-5 outline-none'
+            placeholder='Your name'
+            type='text'
+            required
+          />
+          <input
+            className='w-full h-14 bg-transparent border-2 rounded-xl text-black text-xl px-5 outline-none'
+            placeholder='Your email'
+            type='email'
+            required
+          />
+          <input
+            className='w-full h-14 bg-transparent border-2 rounded-xl text-black text-xl px-5 outline-none'
+            placeholder='Your phone'
+            type='text'
+            required
+          />
+          <input
+            className='w-full h-14 bg-transparent border-2 rounded-xl text-black text-xl px-5 outline-none'
+            placeholder='Your message'
+            type='text'
+            required
+          />
+          <button className='w-52 h-14 bg-black rounded-xl text-white text-xl'>
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
